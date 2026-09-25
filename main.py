5464c7f8090e7c74958d4108bfb1196caf6ec23a
Borys Skiba <borys.00skiba@gmail.com>
2026-09-25 17:10:02 +0200

    Add persistent transaction management

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 import datetime
+import json
+from json import JSONDecodeError
 
 transactions = {}
 
+def load_transactions():
+    global transactions
+
+    try:
+        with open("transactions.json", "r") as f:
+            transactions = json.load(f)
+            print("Successfully loaded transactions.")
+    except JSONDecodeError:
+        print("No saved transactions found.")
+    except FileNotFoundError:
+        print("No transactions found. Creating new transactions file.")
+
 
 def add_transaction():
-    transaction = {"Name": "", "Amount": 0.0, "Type": "", "Date": ""}
+    transaction = {"Name": input("Enter your transaction name: "), "Amount": 0.0, "Type": "", "Date": ""}
 
-    transaction["Name"] = input("Enter your transaction name: ")
+    transaction["Type"] = get_transaction_type()
 
-    while True:
-        ttype = input("Select your transaction type ([i]ncome/[e]xpense): ")
-        if ttype.strip().lower() == "i":
-            transaction["Type"] = "Income"
-            break
-        elif ttype.strip().lower() == "e":
-            transaction["Type"] = "Expense"
-            break
-        else:
-            print("Invalid input. Please enter a valid choice.")
-
-    while True:
-        try:
-            amount = float(input("Enter your transaction amount: "))
-            if amount > 0.0:
-                transaction["Amount"] = amount
-                break
-            else:
-                print("Invalid input. Please enter a positive number.")
-        except ValueError:
-            print("Invalid input. Please enter a number.")
+    transaction["Amount"] = get_transaction_amount()
 
     transaction["Date"] = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S.%f")
 
@@ -39,14 +33,107 @@ def add_transaction():
 
     transactions["t" + tid] = transaction
 
+    save_transactions(transactions)
+
+
+def save_transactions(data):
+    with open("transactions.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def edit_transaction():
+    while True:
+        tid = input("Give an id of the transaction to edit ([c] to cancel): ").strip().lower()
+        if tid in transactions:
+            edited = False
+
+            while not edited:
+                transaction = transactions[tid]
+                print("Editing: " + tid)
+                edit_choice = input("Select the attribute to edit ([n]ame/[t]ype/[a]mount/[c]ancel): ").strip().lower()
+
+                if edit_choice == "n":
+                    new_value = input("Enter new value for name: ")
+                    transaction["Name"] = new_value
+                    edited = True
+                elif edit_choice == "t":
+                    transaction["Type"] = get_transaction_type()
+                    edited = True
+                elif edit_choice == "a":
+                    transaction["Amount"] = get_transaction_amount()
+                    edited = True
+                elif edit_choice == "c":
+                    print("Editing cancelled.")
+                    return
+                else:
+                    print("Invalid input. Please enter a valid choice.")
+            print("Successfully edited: " + tid)
+            save_transactions(transactions)
+            break
+        elif tid == "c":
+            print("Editing cancelled.")
+            return
+        else:
+            print("Transaction not found.")
+
+
+def get_transaction_type():
+    while True:
+        ttype = input("Select your transaction type ([i]ncome/[e]xpense): ")
+        if ttype.strip().lower() == "i":
+            return "Income"
+        elif ttype.strip().lower() == "e":
+            return "Expense"
+        else:
+            print("Invalid input. Please enter a valid choice.")
+
+
+def get_transaction_amount():
+    while True:
+        try:
+            amount = float(input("Enter your transaction amount: "))
+            if amount > 0.0:
+                return amount
+            else:
+                print("Invalid input. Please enter a positive number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+
+def delete_transaction():
+    while True:
+        tid = input("Give an id of the transaction to delete ([c] to cancel): ").strip().lower()
+        if tid in transactions:
+            while True:
+                confirm = input("Are you sure you want to delete the transaction [y/n]: ").strip().lower()
+                if confirm == "y":
+                    transactions.pop(tid)
+                    save_transactions(transactions)
+
+                    print("Transaction successfully deleted.")
+
+                    return
+                elif confirm == "n":
+                    return
+                else:
+                    print("Invalid input. Please enter a valid choice.")
+        elif tid == "c":
+            return
+        else:
+            print("Transaction not found.")
+
 
 def show_transactions():
+    if not transactions:
+        print("No transactions found.")
+        return
+
     total_balance = 0
     total_income = 0
     total_expense = 0
 
     for key, value in transactions.items():
-        print(f"{key}: {value["Type"]}, {value['Amount']} PLN, {value['Date'][:17]}")
+        print(f"{key}: Name: {value["Name"]}, {value["Type"]}, {value['Amount']:.2f} PLN, {value['Date'][:17]}")
 
         if  value["Type"] == "Income":
             total_balance += value['Amount']
@@ -58,19 +145,22 @@ def show_transactions():
     print(f"\nTotal income: {total_income:.2f} PLN\nTotal expense: {total_expense:.2f} PLN\nTotal balance: {total_balance:.2f} PLN")
 
 
-if __name__ == '__main__':
+def main():
+    load_transactions()
+
     print("Welcome to the Financial Analysis Tool.")
+
     while True:
-        print("1. Add transaction.\n2. Show transactions.\n3. Exit.")
+        print("1. Add transaction.\n2. Show transactions.\n3. Edit transaction.\n4. Delete transaction\n5. Exit.")
 
         while True:
             try:
                 choice = int(input("Enter your choice: "))
 
-                if choice in [1, 2, 3]:
+                if choice in [1, 2, 3, 4, 5]:
                     break
                 else:
-                    print("Invalid input. Choose 1, 2 or 3.")
+                    print("Invalid input. Choose 1, 2, 3, 4 or 5.")
 
             except ValueError:
                 print("Invalid input. Please enter a number.")
@@ -80,5 +170,13 @@ if __name__ == '__main__':
         elif choice == 2:
             show_transactions()
         elif choice == 3:
+            edit_transaction()
+        elif choice == 4:
+            delete_transaction()
+        elif choice == 5:
             print("Thank you for using this tool.")
             break
+
+
+if __name__ == '__main__':
+    main()
